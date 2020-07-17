b228618c1995757b18b0297746760e612060377b
hverma4321 <64879832+hverma4321@users.noreply.github.com>
2020-07-17 16:38:52 -0400

    Browser actions

--- content.js
+++ content.js
@@ -2,3 +2,12 @@ alert("Alert!")
 var firstHref = $("a[href^='http']").eq(0).attr("href");
 
 console.log(firstHref);
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+      console.log(firstHref);
+    }
+  }
+);
